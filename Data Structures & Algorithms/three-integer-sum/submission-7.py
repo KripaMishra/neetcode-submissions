dca8fa192e0dd227de4949ce28d16d2a451fa5ba
Kripa Shankar Mishra <143933613+KripaMishra@users.noreply.github.com>
2026-07-25 17:07:53 +0530

    Add: three-integer-sum - submission-7

---
+++ Data Structures & Algorithms/three-integer-sum/submission-7.py
@@ -0,0 +1,20 @@
+class Solution:
+    def threeSum(self,nums: list[int]) -> list[list[int]]:
+        solution = []
+        nums.sort()
+        i = 0
+        while i <= len(nums)-3:
+            j = i +1
+            k = len(nums)-1
+            while j <k :
+                if nums[i]+ nums[j]+ nums[k]==0:
+                    if not [nums[i], nums[j], nums[k]] in solution:
+                        solution.append([nums[i], nums[j], nums[k]])
+                    j+=1
+                    k-=1
+                elif nums[i]+ nums[j]+ nums[k]>0:
+                    k-=1
+                else:
+                    j+=1
+            i+=1
+        return solution
