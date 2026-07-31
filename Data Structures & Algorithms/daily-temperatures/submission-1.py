87d686ef4adf793d6566cdc138cceb2eeb002748
Kripa Shankar Mishra <143933613+KripaMishra@users.noreply.github.com>
2026-07-31 15:18:49 +0530

    Add: daily-temperatures - submission-1

---
+++ Data Structures & Algorithms/daily-temperatures/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        result = []
+        for i in range(len(temperatures)):
+            k =i+1
+            if k>=len(temperatures): result.append(0)
+            while k <len(temperatures):
+                if temperatures[k]>temperatures[i]:
+                    result.append(k-i)
+                    break
+                else:
+                    k+=1
+                    if k>=len(temperatures): result.append(0)
+        return result
